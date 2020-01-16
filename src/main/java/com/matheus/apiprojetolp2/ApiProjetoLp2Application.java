4a5004d41770cff97907de8a33b447f62f170b62
Matheus Matias <matheus.matias.viana@hotmail.com>
2020-01-16 16:14:12 -0300

    projeto criado

---
+++ src/main/java/com/matheus/apiprojetolp2/ApiProjetoLp2Application.java
@@ -0,0 +1,13 @@
+package com.matheus.apiprojetolp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjetoLp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProjetoLp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/apiprojetolp2/ApiProjetoLp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.apiprojetolp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjetoLp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
