6381fcf2fe3b9b1015c74c83f5cecf5aa353f9df
Matheus Matias <matheus.matias.viana@hotmail.com>
2020-02-03 21:14:05 -0300

    ServicoService (in progress...)

---
+++ src/main/java/com/matheus/apiprojetolp2/services/ServicoService.java
@@ -0,0 +1,23 @@
+package com.matheus.apiprojetolp2.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheus.apiprojetolp2.repositories.ServicoRepository;
+
+@Service
+public class ServicoService {
+
+	@Autowired
+	private ServicoRepository servicoRepository;
+	
+	// findAll
+	
+	// findById
+	
+	// insert
+	
+	// update
+	
+	// delete
+}
