a3525c84ce534a34fb0b9209b3d24dc09cf37735
Matheus Matias <matheus.matias.viana@hotmail.com>
2020-02-06 12:41:54 -0300

    ServicoResponseDTO

--- src/main/java/com/matheus/apiprojetolp2/dto/ServicoResponseDTO.java
+++ src/main/java/com/matheus/apiprojetolp2/dto/ServicoResponseDTO.java
@@ -1,9 +1,13 @@
 package com.matheus.apiprojetolp2.dto;
 
+import java.io.Serializable;
+
 import com.matheus.apiprojetolp2.domain.Servico;
 
-public class ServicoResponseDTO {
+public class ServicoResponseDTO implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	private String id;
 	private String nome;
 	private Double custo;
