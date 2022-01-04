54a34c4c8cdff597c35dc4c748d6e8419182f514
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-01-04 10:07:41 -0300

    UserRepository created

---
+++ src/test/java/com/github/matheusmv/apihotel/repositories/UserRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.github.matheusmv.apihotel.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void mustNotBeNull() {
+        assertNotNull(userRepository);
+    }
+}
